5af2304bc631cc32f399bd913992e3aeff989d49
Viacheslav Bulba <viacheslav_bulba@epam.com>
2018-04-07 23:01:02 +0300

    simplify tests

--- src/test/java/tests/ConsumerReportsAZProductsPageTests.java
+++ src/test/java/tests/ConsumerReportsAZProductsPageTests.java
@@ -3,53 +3,38 @@ package tests;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.ConsumerReportsAZProductsPage;
 import pages.ConsumerReportsProductPage;
 import utils.BrowserTestSuite;
-import utils.FileSystem;
 import utils.Logger;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class ConsumerReportsAZProductsPageTests extends BrowserTestSuite {
 
-    private static final String EMPTY_LIST_MESSAGE = "EMPTY LIST OF LINKS";
-
     @DataProvider(parallel = true)
-    public Iterator<Object[]> allLinkTitlesOnAzPage() throws IOException {
-        Document doc = Jsoup.connect(FileSystem.getPropertyFromConfigFile("url")).get();
-        Elements links = doc.select(".products-a-z__results__item");
-        if (links.size() < 1) {
-            ArrayList<Object[]> emptyList = new ArrayList<>();
-            emptyList.add(new Object[]{EMPTY_LIST_MESSAGE});
-            return emptyList.iterator();
-        }
-        return links.subList(0, 9).stream().map(l -> new Object[]{l.text()}).iterator();
+    private Object[][] productsToTest() {
+        return new Object[][]{
+            {"Air conditioners"},
+            {"Airline travel"},
+            {"Bacon"},
+            {"Beer"},
+            {"Batteries"},
+            {"Cameras"},
+            {"Car insurance"},
+            {"Car seats"}
+        };
     }
 
     @Test(description = "Check that links on the A-Z page open appropriate pages",
-        dataProvider = "allLinkTitlesOnAzPage")
+        dataProvider = "productsToTest")
     public void checkAllLinksWork(String linkTitle) throws Exception {
-        if (linkTitle.equals("Air conditioners")) {
-            assertFalse(true);
-        }
-        String linkSingular = linkTitle.endsWith("s") ? linkTitle.substring(0, linkTitle.length() - 1) : linkTitle;
         ConsumerReportsAZProductsPage azPage = new ConsumerReportsAZProductsPage();
-        assertFalse(EMPTY_LIST_MESSAGE.equals(linkTitle), EMPTY_LIST_MESSAGE);
         ConsumerReportsProductPage productPage = azPage.clickOnProductLink(linkTitle);
-        if (linkSingular.equals("Home window")) {
-            linkSingular = "Replacement Windows";
-        }
-        Logger.pass("Check that the header of the opened page contains: " + linkSingular);
-        assertTrue(productPage.getPageHeader().toLowerCase().contains(linkSingular.toLowerCase()));
+        Logger.pass("Check that the header of the opened page contains: " + linkTitle);
+        assertTrue(productPage.getPageHeader().toLowerCase().contains(linkTitle.toLowerCase()));
     }
 
     @Test(description = "There are more than 100 A-Z links on the page")
